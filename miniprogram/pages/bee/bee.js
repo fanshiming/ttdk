eda7f39a0cf1741338ca64ee266c529f28818bac
fanshiming <fanhbu@163.com>
2020-09-15 14:06:50 +0800

    使用weui.wxss 2.4.0 版本 增加bee作为小程序主页

---
+++ miniprogram/pages/bee/bee.js
@@ -0,0 +1,75 @@
+// miniprogram/pages/bee/bee.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    beeTo: [
+      {nickname: '健康码', url: '../index/index'},
+      {nickname: '门架扫描', url: ''},
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  nav_to_nurl: function(event){
+    wx.navigateTo({
+      url: event.target.dataset.nurl,
+    })
+  }
+})
